0a1fc491318b59f5592dd106975a9736defb1a9e
a6888b <ayoubbrahim68@gmail.com>
2023-01-06 12:07:16 +0100

    reglage, ajout des import

--- main.py
+++ main.py
@@ -1,65 +1,19 @@
-import PyPDF4 as pdf
 import cutt_file_parts as cpg
-import argparse
+from args import argument
+from pdf_utils import PDFUtils
 
+args = argument()
+pdf_utils = PDFUtils(args.file)
 
-class PDFUtils:
+if args.mode == "encrypt":
+    pdf_utils.encrypt(args.file, args.password)
 
-    def __init__(self, filename: str):
-        self.filename = filename
+elif args.mode == "decrypt":
+    if args.password:
+        pdf_utils.decrypt(args.password)
+    else:
+        content, length_cpg = cpg.before_last_lenght_file_element(args.wordlists)
+        result_calcul = cpg.calcul(len(content), length_cpg)
 
-    def decrypt(self, passwd: str, file_name_out: str = "out_PDFUtils.pdf"):
-        file_encrypted = pdf.PdfFileReader(self.filename, strict=False)
-        file_out_decrypted = pdf.PdfFileWriter()
-
-        if file_encrypted.isEncrypted:
-            file_encrypted.decrypt(passwd)
-            
-            for index in range(file_encrypted.numPages):
-                page = file_encrypted.getPage(index)
-                file_out_decrypted.addPage(page)
-            
-            print(f"Le mot de passe est {passwd}")
-        with open(file_name_out, "wb") as f:
-            file_out_decrypted.write(f)
-
-    def encrypt(self, pdf_file: str, passwd: str):
-        file = pdf.PdfFileReader(pdf_file, strict=False)
-        out = pdf.PdfFileWriter()
-        num_page = file.numPages
-        
-        for index in range(num_page):
-            page = file.getPage(index)
-            out.addPage(page)
-
-        out.encrypt(passwd)
-        with open(self.filename, "wb") as f:
-            out.write(f)
-
-
-def argument():
-    argument = argparse.ArgumentParser()
-    argument.add_argument("-w", "--wordlists", type=str)
-    argument.add_argument("--mode", type=str, default="decrypt",help="Enter a mode (decrypt or encrypt)")
-    argument.add_argument("-p", '--password', type=str,help="Enter a password if you choice the method encrypt")
-    argument.add_argument("-f", "--file", type=str, required=True)
-    
-    return argument.parse_args()
-
-
-if __name__ == "__main__":
-    args = argument()
-    pdf_utils = PDFUtils(args.file)
-
-    if args.mode == "encrypt":
-        pdf_utils.encrypt(args.file, args.password)
-
-    elif args.mode == "decrypt":
-        if args.password:
-            pdf_utils.decrypt(args.password)
-        else:
-            content, length_cpg = cpg.before_last_lenght_file_element(args.wordlists)
-            result_calcul = cpg.calcul(len(content), length_cpg)
-
-            for content in cpg.cutt(content, result_calcul):
-                cpg.thread_executor(content, pdf_utils.decrypt, 50)
+        for content in cpg.cutt(content, result_calcul):
+            cpg.thread_executor(content, pdf_utils.decrypt, 50)
