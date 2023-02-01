1c5c9031850847280457a29907071a32f21e9932
a6888b <ayoubbrahim68@gmail.com>
2023-02-01 20:41:36 +0100

    add argument: encrypt, decrypt

--- args.py
+++ args.py
@@ -3,7 +3,9 @@ from argparse import ArgumentParser
 def argument():
     argument = ArgumentParser()
     argument.add_argument("-w", "--wordlists", type=str)
-    argument.add_argument("--mode", type=str, default="decrypt",help="Enter a mode (decrypt or encrypt)")
+    argument.add_argument("--encrypt", action='store_true',help="Encrypt")
+    argument.add_argument("--decrypt", action='store_true',help="Decrypt")
+    
     argument.add_argument("-p", '--password', type=str,help="Enter a password if you choice the method encrypt")
     argument.add_argument("-f", "--file", type=str, required=True)
     
